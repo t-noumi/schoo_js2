d6dbe1e6d75405f62dec46173ee066f62f75e86d
t-noumi <tmkq0119@gmail.com>
2022-10-08 11:27:45 +0900

    changre file

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <input type="text" v-model="tag" placeholder="タグを入力">
+    <input type="button" value="検索" @click="getArticles">
+    <ArticleItem
+      v-for="(item, index) in articles"
+      :key="index"
+      :index="index"
+      :article="item"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
+import ArticleItem from './components/ArticleItem.vue'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    ArticleItem
+  },
+  data() {
+    return {
+      articles: [],
+      tag: null
+    }
+  },
+  methods: {
+    async getArticles() {
+      if (this.tag) {
+        const res = await axios.get('https://qiita.com/api/v2/items', {
+          params: {
+            query: `tag:${this.tag}`
+          }
+        })
+        this.articles = res.data
+      }
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/ArticleItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <article>
+    <a :href="article.url" target="_blank">
+      {{ article.title }}
+    </a>
+    <div>
+      <p>by <strong>{{ article.user.id }}</strong></p>
+      <img :src="article.user.profile_image_url" width="20">
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'ArticleItem',
+  props: {
+    article: Object
+  }
+}
+</script>
+/
+<style scoped>
+article {
+  margin-bottom: 20px;
+}
+a, div, p, img {
+  display: inline-block;
+}
+a {
+  font-size: 20px;
+}
+div {
+  margin-left: 8px;
+}
+strong {
+  font-weight: bold;
+}
+img {
+  margin-left: 4px;
+}
+</style>
